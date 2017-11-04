54b9319cf37bd3621a5b11f93a80402ac55596d4
James Royer <walkingalchemy@gmail.com>
2017-11-04 03:07:46 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,117 @@
 # Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
+        "Alan Anderson" => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        "Reggie Evans" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        },
+        "player" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        },
+        "player" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        },
+        "player" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        }
+      }
+    },
+    away: {
+      team_name: "",
+      colors: [],
+      players: {
+        "player" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        },
+        "player" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        },
+        "player" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        },
+        "player" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        },
+        "player" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks:
+        }
+      }
+    }
+  }
+end
