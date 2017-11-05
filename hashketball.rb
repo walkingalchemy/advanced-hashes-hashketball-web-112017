8329085157c860914ca9358f55f3b0937c543cbc
James Royer <walkingalchemy@gmail.com>
2017-11-05 17:47:51 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code here!
+require 'pry'
 def game_hash
   {
     home: {
@@ -16,102 +17,196 @@ def game_hash
           slam_dunks: 1
         },
         "Reggie Evans" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
         },
-        "player" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Brook Lopez" => {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
         },
-        "player" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Mason Plumlee" => {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
         },
-        "player" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Jason Terry" => {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
         }
       }
     },
     away: {
-      team_name: "",
-      colors: [],
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
       players: {
-        "player" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Jeff Adrien" => {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
         },
-        "player" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Bismak Biyombo" => {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
         },
-        "player" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "DeSagna Diop" => {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
         },
-        "player" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Ben Gordon" => {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
         },
-        "player" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Brendan Haywood" => {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
         }
       }
     }
   }
+end
+
+def num_points_scored(player)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |name, stats|
+          if name == player
+            return stats[:points]
+          end
+        end
+      end
+    end
+  end
+end
+
+def shoe_size(player)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |name, stats|
+          if name == player
+            return stats[:shoe]
+          end
+        end
+      end
+    end
+  end
+end
+
+def team_colors(team)
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team
+      return team_data[:colors]
+    end
+  end
+end
+
+def team_names
+  teams = []
+  game_hash.each do |location, team_data|
+    teams << team_data[:team_name]
+  end
+  teams
+end
+
+def player_numbers(team)
+  jersey_numbers = []
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team
+      team_data.each do |attribute, data|
+        if attribute == :players
+          data.each do |name, stats|
+            jersey_numbers << stats[:number]
+          end
+        end
+      end
+    end
+  end
+  jersey_numbers
+end
+
+def player_stats(player)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |name, stats|
+          if name == player
+            return data[name]
+          end
+          # stats[stat] = value
+        end
+      end
+    end
+  end
+  stats
+end
+
+def big_shoe_rebounds
+  biggest = {name: "", size: 0, boards: 0}
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |name, stats|
+          if stats[:shoe] > biggest[:size]
+            biggest[:name] = name
+            biggest[:size] = stats[:shoe]
+            biggest[:boards] = stats[:rebounds]
+          end
+        end
+      end
+    end
+  end
+  biggest[:boards]
 end
